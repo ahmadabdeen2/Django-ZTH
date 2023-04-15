81fe6b59f37aee34b88418d1699a0c79a1d96488
ahmadabdeen2 <ahmadabdin3@hotmail.com>
2023-04-15 05:26:16 +0300

    ignore

---
+++ FourthAppAirbnb/airbnb-next/app/components/navbar/Categories.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Container from '../Container'
+import {TbBeach} from 'react-icons/tb'
+import {GiWindmill} from 'react-icons/gi'
+import {MdOutlineVilla} from 'react-icons/md'
+import CategoryBox from './CategoryBox'
+import { usePathname, useSearchParams } from 'next/navigation'
+
+export const categories = [
+    {
+        label:'Beach',
+        icon: TbBeach,
+        description: "This property has a beach",
+    },
+    {
+        label:"Windmills",
+        icon: GiWindmill,
+        description: "This property has windmills"
+    },
+    {
+        label:"Modern",
+        icon: MdOutlineVilla,
+        description: "This property is modern"
+    }
+]
+
+const Categories = () => {
+    const params = useSearchParams();
+    const selectedCategory = params?.get('category')
+    const pathname = usePathname()
+    const isMainPage = pathname === '/'
+    if (!isMainPage) return null
+  return (
+    <Container>
+        <div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
+             {categories.map((category, index) => (
+                <CategoryBox key={index} {...category}/>
+            ))}
+        </div>
+
+
+    </Container>
+  )
+}
+
+export default Categories
---
+++ FourthAppAirbnb/airbnb-next/app/components/navbar/CategoryBox.tsx
@@ -0,0 +1,74 @@
+import React, { useCallback } from 'react'
+import { IconType } from 'react-icons'
+import { useRouter, useSearchParams } from 'next/navigation'
+import qs from 'query-string'
+
+interface CategoryBoxProps {
+    label: string,
+    icon: IconType,
+    description: string
+    selected?: boolean
+}
+
+
+
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+    label,
+    icon: Icon,
+    description,
+    selected
+
+}) => {
+    const router = useRouter();
+    const params = useSearchParams();
+    const handleClick = useCallback(() => {
+        let currentQuery = {};
+
+        if (params){
+            currentQuery = qs.parse(params.toString())
+        } 
+
+        const updatedQuery: any = {
+            ...currentQuery,
+            category: label
+        }
+
+        if (params?.get('category') === label){
+            delete updatedQuery.category
+        }
+        const url = qs.stringifyUrl({
+            url: '/',
+            query: updatedQuery
+        }, {skipNull: true})
+
+        router.push(url)
+
+
+
+    },[label, params, router])
+  return (
+    <div
+    onClick={handleClick}
+    className={`flex
+    flex-col
+    items-center
+    justify-center
+    cursor-pointer
+    gap-2
+    p-3
+    border-b-2
+    hover:text-neutral-800
+    transition
+    ${selected ? 'border-rose-500 text-rose-500' : 'border-transparent text-neutral-500'}
+    `}
+    >
+        <Icon size={30}/>
+        <div className='font-light text-sm'>
+            {label}
+        </div>
+
+    </div>
+  )
+}
+
+export default CategoryBox
